bead34e9414c364e163b79bef6ab2c67a81e67b5
leoiphonedev <aggarwalaman1985@gmail.com>
2018-12-03 20:32:13 +0530

    Initial Commit

---
+++ SendMailAttachment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SendMailAttachment
+//
+//  Created by Aman Aggarwal on 03/12/18.
+//  Copyright © 2018 Aman Aggarwal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
