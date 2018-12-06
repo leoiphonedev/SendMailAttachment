11cc489da76f2e6f0321b05ba522fbc1714a343b
leoiphonedev <aggarwalaman1985@gmail.com>
2018-12-06 21:48:44 +0530

    Pushing code for the tutorial of sending email in swift iOS app development

--- SendMailAttachment/ViewController.swift
+++ SendMailAttachment/ViewController.swift
@@ -7,12 +7,70 @@
 //
 
 import UIKit
+import MessageUI
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MFMailComposeViewControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    @IBAction func sendEmail(_ sender: Any) {
+        if MFMailComposeViewController.canSendMail() {
+            let mailComposer = MFMailComposeViewController()
+            mailComposer.setSubject("Update about ios tutorials")
+            mailComposer.setMessageBody("What is the update about ios tutorials on youtube", isHTML: false)
+            mailComposer.setToRecipients(["abc@test.com"])
+            guard let filePath = Bundle.main.path(forResource: "SimplePDF", ofType: "pdf") else {
+                return
+            }
+            let url = URL(fileURLWithPath: filePath)
+            
+            do {
+            let attachmentData = try Data(contentsOf: url)
+                mailComposer.addAttachmentData(attachmentData, mimeType: "application/pdf", fileName: "SimplePDF")
+                mailComposer.mailComposeDelegate = self
+                self.present(mailComposer, animated: true
+                    , completion: nil)
+            } catch let error {
+                print("We have encountered error \(error.localizedDescription)")
+            }
+            
+           
+            
+            
+        } else {
+            print("Email is not configured in settings app or we are not able to send an email")
+        }
+    }
+    
+    //MARK:- MailcomposerDelegate
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        switch result {
+        case .cancelled:
+            print("User cancelled")
+            break
+            
+        case .saved:
+            print("Mail is saved by user")
+            break
+            
+        case .sent:
+            print("Mail is sent successfully")
+            break
+            
+        case .failed:
+            print("Sending mail is failed")
+            break
+        default:
+            break
+        }
+        
+        controller.dismiss(animated: true) 
+        
     }
 
 
